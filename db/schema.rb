2f098378d83af865a2047e506177d7008344b13a
kahoshibuya <a14140.ks@gmail.com>
2019-09-11 19:07:31 +0900

    update devise
    deviseの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_144731) do
+ActiveRecord::Schema.define(version: 2019_09_11_095840) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "password", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
