4f267e97c347bcbaa522f25a23d985d325ea6153
kahoshibuya <a14140.ks@gmail.com>
2019-09-12 19:07:26 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-  :registrations => 'users/registrations#new'
-} 
+  devise_for :users, controllers: {
+    registrations:      'users/registrations',
+    sessions:           'users/sessions'
+  }
+  get "users/sign_up", to: "registrations#new"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
