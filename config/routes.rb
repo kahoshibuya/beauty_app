16e4b9ef7bc49a14fa3482e8ed10bd8ff07a50f6
kahoshibuya <a14140.ks@gmail.com>
2019-11-17 13:49:45 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     sessions:           'users/sessions'
   }
   get "users/sign_up", to: "registrations#new"
+  get "users/form_edit", to: "users#edit"
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
