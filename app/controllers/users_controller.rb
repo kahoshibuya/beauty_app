381351b45f4611649472dd46dd947dbafe18b502
kahoshibuya <a14140.ks@gmail.com>
2019-11-17 14:08:14 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def form_edit
     @user = User.find(params[:id])
-    @staff1 = Staff.find(params[:1])
-    @staff2 = Staff.find(params[:2])
+    @staff=Staff.all
+    
   end
 end
